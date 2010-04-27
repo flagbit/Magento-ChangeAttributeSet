baac82b25d2793bcf60197408505a091e054490f
davidfuhr <fuhr@flagbit.de>
2010-04-27 13:15:26 +0000

    git-svn-id: https://svn.dev.flagbit.com/svn/magento-extensions/ChangeAttributeSet/trunk@261 5a9ba00c-e822-4c59-8af6-9966110bd143

---
+++ controllers/IndexController.php
@@ -0,0 +1,66 @@
+<?php
+/*                                                                        *
+ * This script is part of the TypoGento project 						  *
+ *                                                                        *
+ * TypoGento is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU General Public License version 2 as         *
+ * published by the Free Software Foundation.                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
+
+
+require_once 'app/code/core/Mage/Adminhtml/controllers/Catalog/ProductController.php';
+
+/**
+ * ChangeAttributeSet Index Controller
+ *
+ * @version $Id$
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ */
+class Flagbit_ChangeAttributeSet_IndexController extends Mage_Adminhtml_Catalog_ProductController {
+
+	/**
+	 * Class Constructor
+	 * call the parent Constructor
+	 */	
+	public function __constuct(){
+		parent::__construct();
+	}
+	
+	/**
+	 * Product list page
+	 */
+	public function indexAction()
+	{
+
+	    $productIds = $this->getRequest()->getParam('product');
+	    $storeId = (int)$this->getRequest()->getParam('store', 0);
+	    if(!is_array($productIds)) {
+		$this->_getSession()->addError($this->__('Please select product(s)'));
+	    } else {
+		try {
+		    foreach ($productIds as $productId) {
+			$product = Mage::getSingleton('catalog/product')
+			    ->unsetData()
+			    ->setStoreId($storeId)
+			    ->load($productId)
+			    ->setAttributeSetId($this->getRequest()->getParam('attribute_set'))
+			    ->setIsMassupdate(true)
+			    ->save();
+		    }
+		    Mage::dispatchEvent('catalog_product_massupdate_after', array('products'=>$productIds));
+		    $this->_getSession()->addSuccess(
+			$this->__('Total of %d record(s) were successfully updated', count($productIds))
+		    );
+		} catch (Exception $e) {
+		    $this->_getSession()->addError($e->getMessage());
+		}
+	    }
+	    $this->_redirect('adminhtml/catalog_product/index/', array());
+	}	
+	
+}
