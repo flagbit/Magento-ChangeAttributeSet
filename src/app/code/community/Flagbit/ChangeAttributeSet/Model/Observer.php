602ed9e1d197c9cd8b34e49ab5c62f2e354501a8
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-01-30 21:15:16 +0100

    Refactored controller on who to save the new attribute set id. Now we use the collection which loads and save the data more efficiently.

--- src/app/code/community/Flagbit/ChangeAttributeSet/Model/Observer.php
+++ src/app/code/community/Flagbit/ChangeAttributeSet/Model/Observer.php
@@ -20,36 +20,36 @@
  */
 class Flagbit_ChangeAttributeSet_Model_Observer
 {
-	
-	/**
-	 * add Massaction Option to Productgrid
-	 * 
-	 * @param $observer Varien_Event
-	 */
-	public function addMassactionToProductGrid($observer)
-	{
-		$block = $observer->getBlock();
-		if($block instanceof Mage_Adminhtml_Block_Catalog_Product_Grid){
-			
-			$sets = Mage::getResourceModel('eav/entity_attribute_set_collection')
-				->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
-				->load()
-				->toOptionHash();
-		
-			$block->getMassactionBlock()->addItem('flagbit_changeattributeset', array(
-				'label'=> Mage::helper('catalog')->__('Change attribute set'),
-				'url'  => $block->getUrl('*/*/changeattributeset', array('_current'=>true)),
-				'additional' => array(
-					'visibility' => array(
-						'name' => 'attribute_set',
-						'type' => 'select',
-						'class' => 'required-entry',
-						'label' => Mage::helper('catalog')->__('Attribute Set'),
-						'values' => $sets
-					)
-				)
-			)); 			
-		}
-	}
-	
+
+    /**
+     * add Massaction Option to Productgrid
+     *
+     * @param $observer Varien_Event
+     */
+    public function addMassactionToProductGrid($observer)
+    {
+        $block = $observer->getBlock();
+        if ($block instanceof Mage_Adminhtml_Block_Catalog_Product_Grid) {
+
+            $sets = Mage::getResourceModel('eav/entity_attribute_set_collection')
+                ->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
+                ->load()
+                ->toOptionHash();
+
+            $block->getMassactionBlock()->addItem('flagbit_changeattributeset', array(
+                'label'      => Mage::helper('catalog')->__('Change attribute set'),
+                'url'        => $block->getUrl('*/*/changeattributeset', array('_current' => TRUE)),
+                'additional' => array(
+                    'visibility' => array(
+                        'name'   => 'attribute_set',
+                        'type'   => 'select',
+                        'class'  => 'required-entry',
+                        'label'  => Mage::helper('catalog')->__('Attribute Set'),
+                        'values' => $sets
+                    )
+                )
+            ));
+        }
+    }
+
 }
